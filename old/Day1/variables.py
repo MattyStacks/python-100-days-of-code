df3e88715a89a021ec67a5a758e037c2a464327a
MattyStacks <matthew.therault@gmail.com>
2023-09-18 13:15:03 +0000

    finished variables challenge

---
+++ old/Day1/variables.py
@@ -0,0 +1,28 @@
+# name = input("What is your name?")
+# print(name)
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+#Coding excercise soltion.
+# # 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# # 🚨 Don't change the code above 👆
+
+# ####################################
+# #Write your code below this line 👇
+
+# tempa = a
+# tempb = b
+# a = tempb
+# b = tempa
+
+
+# #Write your code above this line 👆
+# ####################################
+
+# # 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
